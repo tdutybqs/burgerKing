08eb4dbd94091e8c3f01cf9246060bc226d5549e
Евгений <35998945+tdutybqs@users.noreply.github.com>
2022-01-24 20:18:03 +0300

    Перевод на функции

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php
 /** @noinspection ALL */
 
-$burgerKingJson =
-    json_decode(
-        file_get_contents('https://burgerking.ru/middleware/bridge/cache/api/v1/menu/coupons?code='),
-        true,
-        512,
-        JSON_THROW_ON_ERROR
-    )['response']['dishes'];
+/**
+ * Вывод на экран всех доступных купонов
+ *
+ * @return void
+ */
+function getCoupons(): void
+{
+    $burgerKingJson =
+        json_decode(
+            file_get_contents('https://burgerking.ru/middleware/bridge/cache/api/v1/menu/coupons?code='),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        )['response']['dishes'];
 
-echo "Найдено " . count($burgerKingJson) . " промокодов\n";
-foreach ($burgerKingJson as $value) {
-    if ("" !== $value['code']) {
-        echo $str = $value['code'] . " " . $value['name'] . " за " . (int)$value['price'] / 100 . "\n";
+    echo "Найдено " . count($burgerKingJson) . " промокодов\n";
+    foreach ($burgerKingJson as $value) {
+        if ("" !== $value['code']) {
+            echo $str = $value['code'] . " " . $value['name'] . " за " . (int)$value['price'] / 100 . "\n";
+        }
     }
 }
+
